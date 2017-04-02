fb84a7463c8e00ec91aa8057bcf0310ecaf77fb5
Kirill Kolesnikov <kkolesnikov@gmail.com>
2017-04-02 19:21:36 +0300

    Rejected commit: Default Initial commit

---
+++ src/test/scala-2.12/integration/AnalyticsTestInt.scala
@@ -0,0 +1,10 @@
+package integration
+
+import org.specs2.mutable.Specification
+
+/**
+  * Created by kkolesnikov on 4/1/2017.
+  */
+class AnalyticsTestInt extends Specification{
+  //ToDo: create integration tests
+}
